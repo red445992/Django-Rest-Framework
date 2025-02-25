ff3e946626401e1fa801c19aaef85cd59383840d
aditya <red445992@gmail.com>
2025-02-25 08:59:26 +0545

    #day2

--- backend/api/views.py
+++ backend/api/views.py
@@ -2,12 +2,20 @@ import json
 from django.forms.models import model_to_dict
 from django.http import JsonResponse,HttpResponse
 from products.models import Product
+from products.serializers import ProductSerializer
+ 
+#4. 
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+
+
+@api_view(['GET','POST'])
 def api_home(request,*args,**kwargs):
     #1. Echo Get Data
     #2. Django Model Instance as an API response
     #3. django model instance to dictionary
-
-
+    #4. rest framework ciew and response
+    #5. drf model serializers
     ######################## 1. Echoing get data##########################################
     # # to access data of params
     # name = request.GET.get('name')
@@ -27,14 +35,23 @@ def api_home(request,*args,**kwargs):
 
 
     #####################################2. django model instance as an API response ##########################
-    model_data = Product.objects.all().order_by("?").first()
+    #4.
+    # if request.method != "POST":
+    #     return Response({'detail':"get not allowed"},status=405)
+    # ####
+    #model_data = Product.objects.all().order_by("?").first()
+    #5. 
+    instance = Product.objects.all().order_by("?").first()
     data = {}
-    if model_data:
+    if instance:
+    #if model_data:
         # data['id'] = model_data.id
         # data['title'] = model_data.title
         # data['content'] = model_data.content
         # data['price'] = model_data.price
-        data = model_to_dict(model_data,fields=['id','title','price']) #3.  model instance to dictionary
-    return JsonResponse(data)
+        #data = model_to_dict(model_data,fields=['id','title','price','sale_price']) #3.  model instance to dictionary
+        data = ProductSerializer(instance).data
+    #return JsonResponse(data)
     # return HttpResponse(data,headers={'content-type':'application/json'}) 
-  
+    #4. 
+    return Response(data)
